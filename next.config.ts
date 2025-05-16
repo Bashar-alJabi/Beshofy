c3998c22cd66c8e3cebf898eb78d1ea78ddc3c38
Bashar <basharaljabi.dev@gmail.com>
2025-05-16 21:59:46 +0400

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  basePath: '/Beshofy',
-  assetPrefix: '/Beshofy',};
+	/* config options here */
+	output: "export",
+	basePath: "/Beshofy",
+	assetPrefix: "/Beshofy/",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
